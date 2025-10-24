0bdfeb82b07ceb0de0519346bf5ae72f1c271359
10410051 <226614468+10410051@users.noreply.github.com>
2025-10-24 01:10:02 +0000

    Update gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0e588171beba0595"; }
+/******/ 		__webpack_require__.h = function() { return "24a8c9c00ca8d522"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
